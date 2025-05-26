9666cd8936d93a8fec0c703e6c1f27736659927f
jayanta2002 <ajayanta99@gmail.com>
2025-05-26 18:21:20 +0530

    Add google link of the image

--- app.py
+++ app.py
@@ -14,20 +14,6 @@ st.image("https://i.postimg.cc/mrsQTCLz/hpp.gif",width=520)
 all_columns=data['data_columns']
 non_location_cols=['total_sqft','bath','bhk']
 location_columns=[col for col in all_columns if col not in non_location_cols]
-st.markdown(
-    """
-    <style>
-    .stApp {
-        background-image: url("file:///C:/Users/KIIT/Downloads/lr.png");
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-attachment: fixed;
-        background-position: center;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 area=st.number_input("__Area(Square Feet)__",min_value=300,max_value=10000,step=50)
 bhk=st.selectbox("BHK",options=[1,2,3,4,5],index=0)
 bath=st.selectbox("Bath Room",options=[1,2,3,4,5],index=0)
